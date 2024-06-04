9ef4f32c8b1dfdeb4102c75137e7063df9d77647
Natan <natanlopes2678@gmail.com>
2024-06-04 18:00:32 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+import numpy as np
+
+
+app = FastAPI()
+
+@app.get("/hello/{valor}")
+async def read(valor):
+    return {"valor": valor}
+
+
+@app.get("/api/{entidade}")
+async def root(entidade):
+
+    ai_model = pickle.load(open("../model.pkl", "rb"))
+
+    file = pd.read_csv("./dados.csv")
+    
+    entidade_especifica = entidade
+
+    filtro = file.loc[file['Entidade'] == entidade_especifica]
+
+    filtro_dict = filtro.to_dict(orient='records')
+
+    X = [
+        filtro_dict[0].get("Lixo plástico mal gerenciado por pessoa (kg por ano)"),
+        filtro_dict[0].get("Participação na emissão global de plásticos para o oceano"),
+        filtro_dict[0].get("Qualidade do Ar")
+    ]
+    
+    filtro = filtro.drop(columns=["Entidade","Ano", "poluicao_da_agua"])
+
+    print(filtro)
+
+    prediction = ai_model.predict(filtro)
+
+
+    match prediction.tolist():
+        case [0]:
+            prediction = "Extremamente sujo"
+        case [1]:
+            prediction = "Limpo"
+        case [2]:
+            prediction = "Muito sujo"
+        case [3]:
+            prediction = "Sujo"
+
+    return {"prediction": prediction, "filtered_data": filtro.to_dict(orient='records')}
